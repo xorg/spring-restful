eff1d2e3d555a4a079a34ec4f6ef90206beb7552
Stefan Schneider <mail@stefanschneider.me>
2021-05-25 19:29:15 +0200

    Rename controller and add working status api endpoint

--- src/main/java/com/springrestful/ApiController.java
+++ src/main/java/com/springrestful/ApiController.java
@@ -1,4 +1,4 @@
-package main.java.com.springrestful;
+package com.springrestful;
 
 
 import java.util.concurrent.atomic.AtomicLong;
@@ -6,14 +6,24 @@ import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 @RestController
-public class RestController {
+public class ApiController {
 
 
 	@GetMapping("/status")
 	public String status() {
 		return "{\"status\": \"ok\"}";
+	}
+
+	@PostMapping(path = "/login",  consumes = "application/json")
+	public ResponseEntity login(@RequestParam(value = "name") String name, @RequestParam(value = "pw") String pw) {
+		if (name == "test" && pw == "1234") {
+			return new ResponseEntity<>(null, HttpStatus.NO_CONTENT);
+		}
+		return new ResponseEntity<>(null, HttpStatus.FORBIDDEN);
 	}
 
 	// @GetMapping("/login")
